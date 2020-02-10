f88f7b5ab3c7944bb854449ecbbc9b4bb202547a
Quentin <Quentin.lecocq@yahoo.com>
2020-02-10 18:47:00 +0100

    Add logic and styling to my navbar

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,10 +1,58 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { FaThinkPeaks } from 'react-icons/fa';
 
-const Navbar = props => {
-  return <div>Navbar here</div>;
+const Navbar = ({ title }) => {
+  return (
+    <nav className='navbar navbar-expand-lg navbar-light bg-light'>
+      <Link to='/' className='navbar-brand'>
+        <FaThinkPeaks /> {title}
+      </Link>
+      <button
+        className='navbar-toggler collapsed'
+        type='button'
+        data-toggle='collapse'
+        data-target='#navbarTogglerDemo02'
+        aria-controls='navbarTogglerDemo02'
+        aria-expanded='false'
+        aria-label='Toggle navigation'
+      >
+        <span className='navbar-toggler-icon'></span>
+      </button>
+
+      <div
+        className='collape navbar-collapse collapse'
+        id='navbarTogglerDemo02'
+      >
+        <ul className='navbar-nav mr-auto mt-2 mt-lg-0'>
+          <li className='nav-item'>
+            <Link to='/' className='nav-link'>
+              Exercises
+            </Link>
+          </li>
+          <li className='navbar-item'>
+            <Link to='/create' className='nav-link'>
+              Create Exercise Log
+            </Link>
+          </li>
+          <li className='navbar-item'>
+            <Link to='/user' className='nav-link'>
+              Create User
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
-Navbar.propTypes = {};
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired
+};
+
+Navbar.defaultProps = {
+  title: 'Exercises Tracker'
+};
 
 export default Navbar;
